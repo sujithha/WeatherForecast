4a41d22040633d242e6040a45c96a6baecefe396
sujithha <sujithha30@gmail.com>
2020-07-31 15:35:50 +0530

    Added reporting changes

---
+++ src/main/java/cucumber/Hooks.java
@@ -0,0 +1,33 @@
+package cucumber;
+
+import BasePackages.BaseTestSession;
+import cucumber.TestContext;
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+
+public class Hooks {
+
+    public static TestContext testContext=null;
+    BaseTestSession session=null;
+   public Hooks(TestContext testContext){
+        this.testContext=testContext;
+        this.session=testContext.getBaseTestSession();
+    }
+    @Before
+    public void beforeSuite(Scenario scenario)  {
+        testContext.setScenario(scenario);
+        session.setScenario(scenario);
+    }
+       
+    @After
+    public void afterSuite(Scenario scenario) throws Throwable {
+       if(scenario.isFailed()){
+           session.takeScreenShot();
+       }
+        session.closeBrowser();
+     }
+    
+}
+
+
